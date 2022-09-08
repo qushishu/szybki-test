bd1e55f80e0954512542bcd5cfe8c88b1c0b984b
jchrustowski <bolek9lodz99@gmail.com>
2022-09-08 06:07:54 +0200

    Added incorrect data labels to login pages

--- my-app/src/components/TeacherLogin.tsx
+++ my-app/src/components/TeacherLogin.tsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 const TeacherLogin = () => {
+  const authorize = ()=>{
+  //  API request
+    setCorrectAuth(false)
+  }
+  const [correctAuth,setCorrectAuth] = useState(true)
   return (
     <div>
       <input type="text" id="login" placeholder="Login" required/>
@@ -8,7 +13,8 @@ const TeacherLogin = () => {
       <div className="forgot_password">
         Nie pamiętam hasła
       </div>
-      <input type="submit" value="Zaloguj"/>
+      <input type="submit" value="Zaloguj" onClick={authorize}/>
+      {correctAuth ? <div>&nbsp;</div>: <div className="incorrect_data_label">Błędne dane logowania</div>}
     </div>
   )
 }
