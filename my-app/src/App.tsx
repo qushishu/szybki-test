8053650c1cc810f64afb1e7c3e9b9f6b259dd688
jchrustowski <bolek9lodz99@gmail.com>
2022-09-08 06:36:26 +0200

    Added react router

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
 import Login from "./components/Login";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
   return (
     <div className="App">
-      <Login/>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={loggedIn ? null : <Navigate replace to="/login"/>}/>
+          <Route path="login" element={<Login/>}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
