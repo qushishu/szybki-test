673f7ce0349e304654e14a6a92b55a51f19c7e3c
qushishu <krzysiek22.00@o2.pl>
2022-09-14 21:43:11 +0200

    save answer in db

--- my-app/src/components/TestSolving.tsx
+++ my-app/src/components/TestSolving.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { fetchQuizQuestions, getQuizQuestionss, Pytanie, Odpowiedz } from './../API';
-import { useNavigate,useLocation } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 
 //Components
 import QuestionCard from './QuestionCard';
@@ -13,45 +13,82 @@ export type AnswerObject = {
   correctAnswer: string;
 }
 
+
+
 const TOTAL_QUESTIONS = 2;
 const TestSolving = () => {
+
 
   const [loading, setLoading] = useState(false);
   const [number, setNumber] = useState(0)
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(false);
   const [answers, setAnswers] = useState<Pytanie[]>([]);
-  const [next,setNext]= useState(false);
-  const [correctAns,setCorrectAns] = useState(0);
-  const [start,setStart] = useState(false);
-  const [userAnswer,setUserAnswer]=useState("");
+  const [next, setNext] = useState(false);
+  const [correctAns, setCorrectAns] = useState(0);
+  const [start, setStart] = useState(false);
+  const [userAnswer, setUserAnswer] = useState("");
 
-  const {state} = useLocation();
-  const testId=(state as {id:number}).id
+  const { state } = useLocation();
+  const testId = (state as { id: number }).id
+  const studentIndeks = (state as { indeks: string }).indeks
+  console.log(studentIndeks)
+  const requestOptionsTest = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ indeks: studentIndeks, test_id: testId })
+  };
+  const odp = (odpId: number) => {
+    const requestOptionsOdp = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ odpowiedz_id: odpId, rozwiazany_test_id: testId })
+    };
+    return requestOptionsOdp
+  }
+
+  const postRozwiazaneTesty = async () => {
+    const endpoint = 'http://localhost:8080/rozwiazanetesty';
+    const data = await (await fetch(endpoint, requestOptionsTest)).json();
+    return data;
+  }
+  const postOdpowiedzStudenta = async (odpId: number) => {
+    const endpoint = 'http://localhost:8080/odpowiedzistudentow';
+    const data = await (await fetch(endpoint, odp(odpId))).json();
+    return data;
+  }
+
+
+
 
   const startTrivia = async () => {
     setLoading(true);
     setStart(true);
+    postRozwiazaneTesty()
     //setstr(await xd());
     const getOdp = await getQuizQuestionss(testId);
     setAnswers(getOdp);
     setScore(0);
     setNumber(0);
     setLoading(false);
+
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
     console.log(e.currentTarget.value);
-    if(!gameOver ){
+    if (!gameOver) {
       const answer = e.currentTarget.value;
-      setUserAnswer("Wybrano: "+answer);
-      for(let i=0;i<4;i++){
-        if(answers[number].odpowiedzi[i].tresc==answer && answers[number].odpowiedzi[i].czyPoprawna){
-            console.log("dobra odpowiedz");
-            setCorrectAns(1);
-            break;
+      setUserAnswer("Wybrano: " + answer);
+      for (let i = 0; i < 4; i++) {
+        if (answers[number].odpowiedzi[i].tresc == answer) {
+          postOdpowiedzStudenta(answers[number].odpowiedzi[i].id)
         }
-        else{
+        if (answers[number].odpowiedzi[i].tresc == answer && answers[number].odpowiedzi[i].czyPoprawna) {
+          console.log("dobra odpowiedz");
+          setCorrectAns(1);
+          break;
+        }
+        else {
           setCorrectAns(0);
         }
       }
@@ -63,12 +100,12 @@ const TestSolving = () => {
     setUserAnswer("");
     setNext(false);
     //move on to the next questions if not the last questio
-    const pkt=score+correctAns;
+    const pkt = score + correctAns;
     setScore(pkt);
     const nextQuestion = number + 1;
     if (nextQuestion === answers.length) {
       setGameOver(true);
-      console.log("pkt:"+pkt);
+      console.log("pkt:" + pkt);
     }
     else {
       setNumber(nextQuestion);
@@ -79,19 +116,19 @@ const TestSolving = () => {
   return (
     <div>
       <h1>REACT QUIZ</h1>
-      
 
-        {gameOver && 
-         <div id="score-div">
+
+      {gameOver &&
+        <div id="score-div">
           <h2>Wynik: {score} / {answers.length} pkt</h2>
           <p >Wysłano wyniki testu do nauczyciela...</p>
-        </div> }
+        </div>}
 
-       {!gameOver &&  !start &&
-          <button className='start' onClick={startTrivia}>
-            Uruchom test
-          </button>
-       }
+      {!gameOver && !start &&
+        <button className='start' onClick={startTrivia}>
+          Uruchom test
+        </button>
+      }
       {/* {!gameOver ? <p className='score'>Score: {score}</p> : null} */}
       {loading && <p id="score-div">Loading Questions ...</p>}
       {!loading && !gameOver && number < answers.length && (
@@ -101,16 +138,16 @@ const TestSolving = () => {
           callback={checkAnswer}
           odpowiedzi={answers}
         />
-          
-        )}
-        
+
+      )}
+
       {!gameOver && !loading && next ? (
         <button className="next" onClick={nextQuestion}>Next Question</button>
       ) : null}
-      
+
       <div id="score-div">
         <p>{userAnswer}</p>
-        </div>
+      </div>
     </div>
   )
 }
