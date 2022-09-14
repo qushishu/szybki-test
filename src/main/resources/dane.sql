743efbee364321a11e35e37fa06e6ddbe7b135a4
qushishu <krzysiek22.00@o2.pl>
2022-09-14 16:56:13 +0200

    data added

---
+++ src/main/resources/dane.sql
@@ -0,0 +1,40 @@
+insert into login (login_id, login, haslo) values (1, 'login', 'haslo');
+insert into login (login_id, login, haslo) values (2, 'teacher1', 'haslo');
+insert into login (login_id, login, haslo) values (3, 'teacher2', 'haslo');
+insert into login (login_id, login, haslo) values (4, 'teacher3', 'haslo');
+insert into login (login_id, login, haslo) values (5, 'teacher4', 'admin');
+
+insert into nauczyciel (nauczyciel_id, name, surname, login_id) values (1, 'Jan', 'Kowalski', 1);
+insert into nauczyciel (nauczyciel_id, name, surname, login_id) values (2, 'Marcin', 'Kowalski', 2);
+insert into nauczyciel (nauczyciel_id, name, surname, login_id) values (3, 'Pawel', 'Marceli', 3);
+insert into nauczyciel (nauczyciel_id, name, surname, login_id) values (4, 'Agnieszka', 'Czad', 4);
+insert into nauczyciel (nauczyciel_id, name, surname, login_id) values (5, 'Natalia', 'Balwan', 5);
+
+insert into test (czas_trwania, data_uruchomienia, data_zakonczenia, nazwa, token, nauczyciel_id) values ('2022-09-14 00:30:00', '2022-09-14', '2022-09-16', 'test1', 'token', 1);
+insert into test (czas_trwania, data_uruchomienia, data_zakonczenia, nazwa, token, nauczyciel_id) values ('2022-09-14 00:30:00', '2022-09-14', '2022-09-16', 'test2', 'token2', 1);
+
+insert into pytanie (pytanie_id, tresc, test_id) values (1, 'ile to 2+2?', '1');
+insert into pytanie (pytanie_id, tresc, test_id) values (2, 'ile to 2+3?', '1');
+
+insert into pytanie (pytanie_id, tresc, test_id) values (3, 'ile masz lat?', '2');
+insert into pytanie (pytanie_id, tresc, test_id) values (4, 'ile masz wzrostu?', '2');
+
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (true, '4', 1);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '5', 1);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '6', 1);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '0', 1);
+
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (true, '4', 2);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '5', 2);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '6', 2);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '0', 2);
+
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (true, '4', 3);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '5', 3);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '6', 3);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '0', 3);
+
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (true, '4', 4);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '5', 4);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '6', 4);
+insert into odpowiedz (czy_poprawna, tresc, pytanie_id) values (false, '0', 4);
